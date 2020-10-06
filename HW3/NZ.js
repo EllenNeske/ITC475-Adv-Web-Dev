540d359f01c18c800e4d033a68142af2d873a472
Ellen Neske <70030075+EllenNeske@users.noreply.github.com>
2020-10-06 00:17:05 -0500

    Add files via upload

---
+++ HW3/NZ.js
@@ -0,0 +1,27 @@
+function NZ() {
+	return(
+		<div>
+			<p>New Zealand</p>
+			
+			<input type = "chekbox" />
+			<p>"City Tours"</p>
+			
+			<input type = "chekbox" />
+			<p>"Sports"</p>
+			
+			<input type = "chekbox" />
+			<p>"Cycling"</p>
+
+			<input type = "chekbox" />
+			<p>"Museums"</p>
+
+			<input type = "chekbox" />
+			<p>"Boating"</p>			
+		</div>
+		);
+	}
+}
+
+export default NZ
+
+
---
+++ HW3/Venice.js
@@ -0,0 +1,22 @@
+function Venice() {
+	return(
+		<div>
+			<p>Venice</p>
+		<div>
+			<input type = "chekbox" />
+			<p>"Museums"</p>
+			
+			<input type = "chekbox" />
+			<p>"Theatre"</p>
+			
+			<input type = "chekbox" />
+			<p>"Parks and Recreation"</p>
+
+			<input type = "chekbox" />
+			<p>"City Tours"</p>		
+		</div>
+		);
+	}
+}
+
+export default Venice
