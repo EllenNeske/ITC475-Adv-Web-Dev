655ff12efe850fc2d3fe7d12e65468e68278aeaa
Ellen Neske <70030075+EllenNeske@users.noreply.github.com>
2020-11-09 23:07:05 -0600

    Add files via upload

---
+++ PHP HW/confirm.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+Thank you for submitting your travel agent contact request! Here is the information you submitted:
+
+Client Name: <?php echo $_POST["First Name " + "Last Name"]; ?><br>
+Client Phone Number: <?php echo $_POST["phoneNumber"]; ?><br>
+Client Email: <?php echo $_POST["emailAddress"]; ?><br>
+Number of Adults: <?php echo $_POST["Number of Adults"]; ?><br>
+Number of Children: <?php echo $_POST["Number of Children"]; ?><br>
+Destination: <?php echo $_POST["Destination"]; ?><br>
+Travel Dates: <?php echo $_POST["Trip Date"]; ?><br>
+Interested Activities: <?php echo $_POST[""]; ?><br>
+
+An agent will be in touch with you soon!
+</body>
+</html>
