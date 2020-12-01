25f17d3e8d61ec11604da1320896144d5361deb2
Ellen Neske <70030075+EllenNeske@users.noreply.github.com>
2020-11-30 20:06:42 -0600

    Add files via upload

---
+++ PHP HW/admin.php
@@ -0,0 +1,28 @@
+<?php
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname, phonenumber, email, adults, children, destination, dates, activities FROM MyClients";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "Client Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>"
+	echo "Client Phone Number: " . $row["phonenumber"]. "<br>"
+	echo "Client Email: " . $row["email"]. "<br>"
+	echo "Number of Adults: " . $row["adults"]. "<br>"
+	echo "Number of Children: " . $row["children"]. "<br>"
+	echo "Destination: " . $row["destination"]. "<br>"
+	echo "Travel Dates: " . $row["dates"]. "<br>"
+	echo "Interested Activities: " . $row["activities"]. "<br>"
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
--- PHP HW/confirm.php
+++ PHP HW/confirm.php
@@ -12,5 +12,54 @@ Travel Dates: <?php echo $_POST["Trip Date"]; ?><br>
 Interested Activities: <?php echo $_POST[""]; ?><br>
 
 An agent will be in touch with you soon!
+
+<?php
+// Create database
+$sql = "CREATE DATABASE FormData";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+
+// sql to create table
+$sql = "CREATE TABLE MyClients (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+phonenumber CHAR(10) NOT  NULL,
+email VARCHAR(50) NOT NULL,
+adults INT(2),
+children INT(2),
+destination VARCHAR(30)
+dates DATE,
+activities VARCHAR"
+)";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Table MyClientss created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+
+// Insert data
+
+$sql = "INSERT INTO MyClients (firstname, lastname, phonenumber, email, adults, children, destination, dates, activities)
+VALUES (echo 'First Name', echo 'Last Name', echo 'phoneNumber', echo 'emailAddress', echo 'Number of Adults', echo 'Number of Children', echo 'Destination', echo 'Trip Date', echo 'Activities')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+?>
 </body>
 </html>
